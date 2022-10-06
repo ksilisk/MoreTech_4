5ae7cfbe751c15e06e2b7c6a9fd96ac004ed9e27
Shaliko <shaliko9@yandex.ru>
2022-10-06 14:56:36 +0300

    prototype for vedomosti

--- parser.py
+++ parser.py
@@ -1,4 +1,12 @@
+'''
+Тут класс, в котором реализованы функции парсинга новостных сайтов
+'''
+
 class Parser:
 
     def __init__(self) -> None:
+        self.sites = {'1': 'https://www.vedomosti.ru/'}
+
+    # парсинг сайта ведомостей
+    def vedomosti(count: int) -> dict:
         pass
