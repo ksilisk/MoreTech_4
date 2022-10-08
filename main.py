23d8f7517822b718e2f62ace4945a349da4d3b42
Shaliko <shaliko9@yandex.ru>
2022-10-08 19:56:20 +0300

    add klerk.ru and start work for telegram chanels

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from parser import Parser
+import json
+
+
+# buh_ru id = 4830
+# klerk id = 7300
 
 def main():
-    a = Parser()
+    parser = Parser()
+    back_data = {}
+    with open('result.json', 'r') as f:
+        back_data = json.loads(f.read())
+    f.close()
+    new_data = {'buh.ru': parser.buh_ru(4830, 50), 'klerk.ru': parser.klerk_ru(7300, 0)}
+    for key in back_data.keys():
+        new_data[key].extend(back_data[key])
+    with open('result.json', 'w') as f:
+        json.dump(new_data, f, ensure_ascii=False)
+    f.close()
+
 
 if __name__ == '__main__':
     main()
--- parser.py
+++ parser.py
@@ -1,25 +1,31 @@
 '''
 Тут класс, в котором реализованы функции парсинга новостных сайтов
 '''
+from telethon import TelegramClient, sync
 from bs4 import BeautifulSoup
 import requests
 import json
 import feedparser
 import time
 
+
 class Parser:
 
     def __init__(self) -> None:
         with open('sites.json', 'r') as f:
             self.sites = json.loads(f.read())
+        f.close()
+        with open('api.json', 'r') as f:
+            self.api_data = json.loads(f.read())
+        f.close()
 
-    #  парсинг сайта ведомостей
-    def buh_ru(self, count: int) -> list:
+    #  парсинг сайта buh.ru
+    def buh_ru(self, start: int, count: int) -> list:
         last_news_id = int(feedparser.parse(self.sites['buh_ru'])['entries'][0]['links'][0]['href'].split('/')[5])
-        if count >= last_news_id:
+        if count >= last_news_id or count == 0:
             return []
         result = []
-        for i in range(last_news_id - count, last_news_id):
+        for i in range(last_news_id - start - count, last_news_id - start):
             page = requests.get('https://buh.ru/news/uchet_nalogi/' + str(i))
             if page.status_code == 200:
                 soup = BeautifulSoup(page.text, 'html.parser')
@@ -33,12 +39,37 @@ class Parser:
                 time.sleep(3)
         return result
 
+    #  парсинг сайта klerk.ru
+    def klerk_ru(self, start: int, count: int) -> list:
+        last_news_id = int(feedparser.parse(self.sites['klerk_ru'])['entries'][0]['links'][0]['href'].split('/')[5])
+        if count >= last_news_id or count == 0:
+            return []
+        result = []
+        for i in range(last_news_id - start - count, last_news_id - start):
+            page = requests.get('https://www.klerk.ru/buh/news/' + str(i))
+            if page.status_code == 200:
+                soup = BeautifulSoup(page.text, 'html.parser')
+                title = soup.find('header', 'article__header').find('h1').text
+                date = soup.find('span', 'status__block').find('core-date-format')['date']
+                text_data = soup.find('div', 'article__content').find_all()
+                text = ''
+                for j in text_data:
+                    if j.name == 'p':
+                        text += j.text + '\n'
+                text = text.replace("\t", "").replace("\r", "").replace("\xa0", " ")
+                result.append({'title': title, 'date': date, 'text': text})
+        return result
+
+    #  парсинг телеграмм каналов
+    def parse_telegram_channel(self, count: int) -> list:
+        client = TelegramClient('Parser', self.api_data['api_id'], self.api_data['api_hash'])
+
 
 def main():
     parser = Parser()
-    a = parser.buh_ru(10)
+    a = parser.klerk_ru(0, 10)
     for i in a:
-        print(i['date'] + '\n', i['text'])
+        print(i)
 
 
 if __name__ == '__main__':
